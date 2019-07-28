e87c2f04593c8f7e3a5c1c66260c49a3690db90c
rushdi21-meet <rushdi21@meet.mit.edu>
2019-07-28 11:36:46 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,10 @@
+i=333
+def add_numbers(start, end):
+    #write the body of this
+    #function, similar to the block
+    #of code we just saw. Hint:
+    #don’t forget to use return
+    
+    answer =add_numbers(i,i+=1)
+    i+=1
+print(answer)
--- nice_paintig.py
+++ nice_paintig.py
@@ -1,11 +1,14 @@
 import turtle
-red = turtle.clone()
-pink =turtle.clone()
-green= turtle.clone()
-yellow = turtle.clone()
-orange = turtle.clone()
-blue = turtle.clone()
-purple=turtle.clone()
+color=["green", "yellow",'orange','blue','pruple','red','pink']
+x=10
+y= 270
+i=0
+turtle.bgcolor("black")
 while True:
-    green =
+    turtle.color(color[0])
+    turtle.forward(x)
+    turtle.left(y)
+    x+=10
+    y-=1
+    i+=1
 turtle.mainloop()
